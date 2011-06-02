5d6c4810674d6297400c3591b49c80878032b7a9
Matt Basta <me@mattbasta.com>
2011-06-02 15:52:46 -0700

    Updated default preferences with message for removal (bug 625055)

--- packager/resources/defaults/preferences/prefs.js
+++ packager/resources/defaults/preferences/prefs.js
@@ -1,3 +1,8 @@
+// Default preference values. These are accessible via the preferences system
+// or via the optional chrome/content/options.xul preferences dialog.
+
+// These are here as an example only. Please remove them or rename them to
+// something useful.
 pref("extensions.%slug%.boolpref", false);
 pref("extensions.%slug%.intpref", 0);
 pref("extensions.%slug%.stringpref", "A string");
